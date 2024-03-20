f5639537d65f1cbb2e18bfe163865686ea53e9ce
BastianStruggl <b.struggl@it-frankfurt.com>
2024-03-20 11:56:08 +0100

    -

--- src/main/java/com/mybytes/springsection2/MyBytesBackendApplication.java
+++ src/main/java/com/mybytes/springsection2/MyBytesBackendApplication.java
@@ -3,8 +3,10 @@ package com.mybytes.springsection2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 //@ComponentScan("com.mybytes.springsection2.controller") //Optional - @SpringBootApplication contains component scan
 public class MyBytesBackendApplication {
 
